8120ff0ac744c6bbc6b80e430167b095838f5b04
khalil6c <cissekhalil15@gmail.com>
2023-07-24 01:53:10 +0000

    implementation de l'interface admin

--- backend/api/services/immobilierService.js
+++ backend/api/services/immobilierService.js
@@ -9,22 +9,22 @@ const pool = new Pool({
 });
 
 const immobilierService = {
-    async getAll() {
-        try {
-          const query = 'SELECT * FROM immobilier';
-          const result = await pool.query(query);
-          return result.rows;
-        } catch (error) {
-          throw error;
-        }
+  async getAll() {
+    try {
+      const query = 'SELECT * FROM immobilier';
+      const result = await pool.query(query);
+      return result.rows;
+    } catch (error) {
+      throw error;
+    }
   },
 
   async create(data) {
     try {
       const { title, price, location, image } = data;
       const query =
-        'INSERT INTO immobilier (title, price, location, image) VALUES ($1, $2, $3, $4) RETURNING *';
-      const values = [title, price, location, image];
+        'INSERT INTO immobilier (title, price, location, image, etat) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+      const values = [title, price, location, image, true]; // Nouveaux biens ont un état "true" par défaut
       const result = await pool.query(query, values);
       return result.rows[0];
     } catch (error) {
@@ -48,33 +48,29 @@ const immobilierService = {
 
   async update(id, data) {
     try {
-      const { title, price, location, image } = data;
-      const query =
-        'UPDATE immobilier SET title = $1, price = $2, location = $3, image = $4 WHERE id = $5 RETURNING *';
-      const values = [title, price, location, image, id];
-      const result = await pool.query(query, values);
-      if (result.rows.length === 0) {
+      // Vérifier si le bien immobilier avec l'ID spécifié existe
+      const existingImmobilier = await this.getById(id);
+
+      if (!existingImmobilier) {
         throw new Error('Aucun bien immobilier trouvé avec cet ID');
       }
+
+      // Fusionner les nouvelles données avec les anciennes données pour ne mettre à jour que les propriétés fournies
+      const updatedImmobilier = { ...existingImmobilier, ...data };
+
+      // Supprimer l'ID du nouvel objet de bien immobilier, car nous ne voulons pas mettre à jour l'ID de la ligne
+      delete updatedImmobilier.id;
+
+      const { title, price, location, image, etat } = updatedImmobilier;
+      const query =
+        'UPDATE immobilier SET title = $1, price = $2, location = $3, image = $4, etat = $5 WHERE id = $6 RETURNING *';
+      const values = [title, price, location, image, etat, id];
+      const result = await pool.query(query, values);
       return result.rows[0];
     } catch (error) {
       throw error;
     }
   },
-
-  async delete(id) {
-    try {
-      const query = 'DELETE FROM immobilier WHERE id = $1 RETURNING *';
-      const values = [id];
-      const result = await pool.query(query, values);
-      if (result.rows.length === 0) {
-        throw new Error('Aucun bien immobilier trouvé avec cet ID');
-      }
-      return result.rows[0];
-    } catch (error) {
-      throw error;
-    }
-  }
 };
 
 module.exports = immobilierService;
