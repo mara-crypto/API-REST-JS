9539755fb0560fb076cccc29f18ca55f92061d56
mara-cryto <serignemodoudiop55@gmail.com>
2023-07-24 13:59:45 +0000

    merge

--- frontend/js/affiche_page.js
+++ frontend/js/affiche_page.js
@@ -15,6 +15,6 @@ const role = parseJwt(token).role;
 if (role === "editeur"){
     href_proposition.setAttribute("href","proposition.html");
 } else {
-    href_tableau_de_bord.setAttribute("href", "tableau_de_bord.html");
+    href_tableau_de_bord.setAttribute("href", "dashboard/index.html");
 };
        
